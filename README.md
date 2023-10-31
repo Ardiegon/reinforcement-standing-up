15f7921c61f798a41be77a76fdadba497244d710
Ardiegon <oskbsz@gmail.com>
2023-10-31 13:22:25 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # USD-2023Z
 Reinforcement learning with PyBullet simulator
+
+## Instalation
+Exec instructions given here: https://github.com/openai/mujoco-py/tree/master
+then run 
+```bash
+pip install -r requirements.txt
+```
+
---
+++ notebooks/test_sim.py
@@ -0,0 +1,19 @@
+# simple script to test our humanoid
+
+import gym
+from gym.utils.env_checker import check_env
+
+env = gym.make("HumanoidStandup-v4", render_mode="human")
+check_env(env)
+
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
